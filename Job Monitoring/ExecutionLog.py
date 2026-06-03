0c827f4941a6e80f3984d60b8e782ce727fc0134
Daniel Schwammlein <d.g.schwammlein@wustl.edu>
2026-06-03 09:30:22 -0500

    Provenance log v1.0 (base template) added

--- Job Monitoring/ExecutionLog.py
+++ Job Monitoring/ExecutionLog.py
@@ -1,16 +1,18 @@
+# EXECUTION LOG v1.0, DANIEL SCHWAMMLEIN
 import json
 import sqlite3
 from datetime import datetime, timezone
 DB_NAME = 'executionlog.db'
 
 
-# Initializes the database
-def init_db():
+# Initializes the database, called once
+def initDb():
     con = sqlite3.connect(DB_NAME)
     cur = con.cursor()
     cur.execute('''CREATE TABLE jobs(
                 submissionId TEXT PRIMARY KEY,
                 jobName TEXT NOT NULL,
+                user TEXT NOT NULL,
                 submitTime TEXT NOT NULL,
                 startTime TEXT,
                 endTime TEXT,
@@ -23,50 +25,55 @@ def init_db():
 # Gets the status of a job from an id
 def status(specifiedJobId):
     # Opening database and getting status of specified job
-    con = sqlite3.connect(DB_NAME)
-    cur = con.cursor()
-    query = '''SELECT status FROM jobs WHERE submissionId = ?'''
-    cur.execute(query, (specifiedJobId,))
-    result = cur.fetchone()
+    with sqlite3.connect(DB_NAME) as con:
+        cur = con.cursor()
+        query = '''SELECT status FROM jobs WHERE submissionId = ?'''
+        cur.execute(query, (specifiedJobId,))
+        result = cur.fetchone()
 
-    # Closing database
-    con.commit()
-    con.close()
+        # Closing database
+        con.commit()
+        con.close()
 
-    # Returning result
-    if result is None:
-        return "No job matching specified job id"
-    else:
-        return result[0]
+        # Returning result
+        if result is None:
+            return "No job matching specified job id"
+        else:
+            return result[0]
 
+# New job added to SQL database, should be called whenever the job is queued
 def jobQueued(submissionId):
-    # submissionId is also the name of the file containing data
-    file = open(f'../SubmissionFiles/{submissionId}.json', 'r')
+    # Load file data
+    file = open(f'Logs/Task_Logs/{submissionId}.json', 'r') # submissionId is also the name of the file containing data
     data = json.load(file)
     _submissionId = data["submissionId"]
+    _user = data["user"]
     _submitTime = data["submitTime"]
     _jobName = data["jobName"]
     _metadata = json.dumps(data.get("metadata",{}))
-    with sqlite3.connect(DB_NAME) as con:
 
+    # Add it to the sql database
+    with sqlite3.connect(DB_NAME) as con:
         cur = con.cursor()
-        cur.execute('''INSERT INTO jobs(submissionId, jobName, submitTime, metadata, status) VALUES(?,?,?,?,?)''',
-                    (_submissionId, _jobName, _submitTime, _metadata, "QUEUED"))
+        cur.execute('''INSERT INTO jobs(submissionId, user, jobName, submitTime, metadata, status) VALUES(?,?,?,?,?)''',
+                    (_submissionId, _user, _jobName, _submitTime, _metadata, "QUEUED"))
         con.commit()
 
-#
+# Update job's status to RUNNING and startTime to present
 def jobStarted(submissionId):
     with sqlite3.connect(DB_NAME) as con:
         cur = con.cursor()
         cur.execute('''UPDATE jobs SET startTime = ?, status = 'RUNNING' WHERE submissionId = ?''',(datetime.now(timezone(timezone.utc).isoformat),submissionId,))
         con.commit()
 
+# Update job's status to SUCCESS and endTime to present
 def jobCompleted(submissionId):
     with sqlite3.connect(DB_NAME) as con:
         cur = con.cursor()
         cur.execute('''UPDATE jobs SET endTime = ?, status = 'SUCCESS' WHERE submissionId = ?''',(datetime.now(timezone(timezone.utc).isoformat),submissionId,))
         con.commit()
 
+# Update job's status to FAILURE and add errorMessage
 def jobFailed(submissionId, errorMessage):
     with sqlite3.connect(DB_NAME) as con:
         cur = con.cursor()
---
+++ Job Monitoring/ProvenanceStore.py
@@ -0,0 +1,45 @@
+# PROVENANCE LOG v1.0, DANIEL SCHWAMMLEIN
+
+import sqlite3
+import json
+from datetime import datetime, timezone
+
+DB_NAME = 'provenance.db'
+
+# Initializing databse
+def initProvenance():
+    with sqlite3.connect(DB_NAME) as con:
+        cur = con.cursor()
+
+        # Creating the database
+        cur.execute('''CREATE TABLE provenance(
+        provenanceId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        submissionId INTEGER NOT NULL FOREIGN KEY, -- corresponds to submissionId for execution log
+        user VARCHAR NOT NULL,
+        assumptions TEXT,
+        decisions TEXT,
+        
+        
+        ''')
+    return
+
+# Data is given to the provenance log as a python dictionary, dumped from a json file by provenance capture service
+# Returns reference id for provenance log
+def logProvenance(data):
+    with sqlite3.connect(DB_NAME) as con:
+        cur = con.cursor()
+
+        # Load data
+        _submissionId = data["submissionId"]
+        _user = data["user"]
+        _assumptions = data["assumptions"]
+        _decisions = data["decisions"]
+
+        # Add data to SQL database
+        cur.execute("INSERT INTO provenance(submissionId,user,assumptions,decisions) VALUES (?,?,?,?)",
+                    (_submissionId, _user, _assumptions, _decisions))
+        con.commit()
+        return cur.lastrowid
+
+
+
