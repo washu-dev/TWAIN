1141f49d30f9b542b937561a90aea650027dea52
Daniel Schwammlein <d.g.schwammlein@wustl.edu>
2026-06-08 12:02:28 -0500

    Speech Recognition updated. Flows better, formatted in object oriented, can toggle listening

--- SpeechRecognition/SpeechRecognition.py
+++ SpeechRecognition/SpeechRecognition.py
@@ -1,53 +1,61 @@
+# Speech Recognition v1.1
 import os
 import speech_recognition as sr
 from pynput import keyboard
 import time
+class SpeechRecognition:
+    def __init__(self):
+        self.listening = True
+        self.transcription = []
+        self.running = True
 
-listening = True
-transcription = []
+        self.rec = sr.Recognizer()
+        self.mic = sr.Microphone()
 
-def listen(recognizer, audio):
-    global transcription
-    try:
-        transcription.append(recognizer.recognize_google(audio))
-        print("appending")
-    except sr.UnknownValueError:
-        print("Could not understand audio")
-    except sr.RequestError as e:
-        print(f"API error: {e}")
-
-def endListening():
-    global listening
-    listening = False
-
-def on_press(key):
-    global listening
-    if key == keyboard.Key.space:
-        listening = False
-        return False
-
-def main():
-    global listening
-    global transcription
-    rec = sr.Recognizer()
-    mic = sr.Microphone()
-    print(sr.Microphone.list_microphone_names())
-    with mic as source:
         print("Please be silent for a second to collect ambience noise")
-        rec.adjust_for_ambient_noise(source)
+        time.sleep(0.5)
+        with self.mic as source:
+            self.rec.adjust_for_ambient_noise(source)
+        print("CONTROLS")
+        print("Space to toggle listening")
+        print("Left Shift to print transcript")
+        print("ESC to quit")
+        print("LISTENING...")
+    def audioCallback(self, recognizer, audio):
+        if(not self.listening):
+            return
+        else:
+            try:
+                text = recognizer.recognize_google(audio)
+                self.transcription.append(text)
+                print(f"live output: {text}")
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError as e:
+                print(f"API error: {e}")
 
-    backgroundRecorder = rec.listen_in_background(mic, listen)
-    keyboardListener = keyboard.Listener(on_press=on_press)
-    keyboardListener.start()
-    while listening:
-        time.sleep(0.2)
-    backgroundRecorder(wait_for_stop = False)
-    keyboardListener.stop()
-
-    if transcription:
-        fullScript = " ".join(transcription)
-        print(fullScript)
+    def on_press(self,key):
+        if(key == keyboard.Key.space):
+            self.listening = not self.listening
+            if(self.listening): print("LISTENING...")
+            else: print("PAUSED")
+        elif(key == keyboard.Key.shift_l):
+            self.printTranscript()
+        elif(key == keyboard.Key.esc):
+            running = False
+            return False
+    def printTranscript(self):
+        if(self.transcription): print(" ".join(self.transcription))
+        else: print("No transcript")
+    def run(self):
+        background_listener = self.rec.listen_in_background(self.mic, self.audioCallback)
+        with keyboard.Listener(on_press=self.on_press) as listener:
+            while(self.running):
+                time.sleep(0.1)
+        background_listener(wait_for_stop=False)
+        self.printTranscript()
 
 
 if __name__ == "__main__":
-    main()
+    app = SpeechRecognition()
+    app.run()
